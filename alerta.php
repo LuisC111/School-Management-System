12b2eb741a265d129816de5982750cdca7b2528a
Luis <52750899+LuisC111@users.noreply.github.com>
2021-07-19 23:45:01 -0500

    Add files via upload

---
+++ alerta.php
@@ -0,0 +1,8 @@
+
+           <?php 
+           
+              echo "<script>jQuery(function(){
+                swal(\"Bienvenido al C.A..R.P\", \"Has iniciado sesion correctamente\", \"success\");
+              });</script>";
+            
+          ?>
