d105beff0781e5396d0e4640138ec549fd3458a1
Luis <52750899+LuisC111@users.noreply.github.com>
2021-07-19 23:45:43 -0500

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+    <div class="footerBottomSection">
+        <div class="container">
+            <center>© 2020, Todos los Derechos Reservados. C.A.R.P<a href="#"> GUZMAN'S WEB</a></center>
+        </div>
+    </div>
